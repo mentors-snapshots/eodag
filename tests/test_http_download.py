99b98ed4c2a7dca2b066fba507036aedd63b9f49
Devin <nrdagar@gmail.com>
2024-12-16 22:23:52 +0530

    adding http headers

--- tests/test_http_download.py
+++ tests/test_http_download.py
@@ -1,1 +1,30 @@
- 
+import pytest
+from plugins.http_download import HTTPDownload
+
+def test_get_rio_env_with_headers():
+    """Test get_rio_env when headers are provided"""
+    headers = {
+        'Authorization': 'Bearer token123',
+        'Accept': 'application/json'
+    }
+    
+    plugin = HTTPDownload(headers=headers)
+    env = plugin.get_rio_env()
+    
+    assert 'GDAL_HTTP_HEADERS' in env
+    expected_headers = "Authorization: Bearer token123\r\nAccept: application/json"
+    assert env['GDAL_HTTP_HEADERS'] == expected_headers
+
+def test_get_rio_env_without_headers():
+    """Test get_rio_env when no headers are provided"""
+    plugin = HTTPDownload(headers=None)
+    env = plugin.get_rio_env()
+    
+    assert env == {}
+
+def test_get_rio_env_empty_headers():
+    """Test get_rio_env when headers dict is empty"""
+    plugin = HTTPDownload(headers={})
+    env = plugin.get_rio_env()
+    
+    assert env == {} 
